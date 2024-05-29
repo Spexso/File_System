ff8dae16040bc68e2de561351e8dcdabb65c3fbb
Kağan <89082422+Spexso@users.noreply.github.com>
2024-05-30 01:47:14 +0300

    .

--- src/initFileSystem.cpp
+++ src/initFileSystem.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    std::cout << "FileSystem is alive " << std::endl ; 
+    std::cout << "FileSystem is alive " << std::endl;
+    std::cout << "FileSystem is alive " << std::endl; 
+
 }
