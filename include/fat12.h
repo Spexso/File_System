a2ebcd3cbadf108fa9f372c647c70e775f8b3f7e
Kağan <89082422+Spexso@users.noreply.github.com>
2024-06-01 22:55:10 +0300

    Fat12 Header

--- include/fat12.h
+++ include/fat12.h
@@ -1,4 +1,55 @@
 #ifndef FAT12_H_
 #define FAT12_H_
 
+
+typedef struct
+{
+    /* data */
+}BootBlock;
+
+// Contains Meta Data about File System
+typedef struct
+{
+    /* data */
+}SuperBlock;
+
+// Contains A single block in the File System
+typedef struct
+{
+    /* data */
+}Block;
+
+// 
+typedef struct
+{
+    /* data */
+}File;
+
+// 
+typedef struct
+{
+    /* data */
+}Directory;
+
+typedef struct
+{
+    /* data */
+}DirectorySystem;
+
+// File System itself
+typedef struct 
+{
+    /* data */
+}FileSystem;
+
+
+class Fat12Table
+{
+    public:
+
+    private:
+
+};
+
+
 #endif // FAT12_H_
